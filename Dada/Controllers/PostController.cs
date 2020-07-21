55dea6519216ca2d6d9fc8287cf769633d02848a
nurlaney <nurlaney@code.edu.az>
2020-07-21 15:17:01 +0400

    SomeCommentTests

--- Dada/Controllers/PostController.cs
+++ Dada/Controllers/PostController.cs
@@ -7,6 +7,7 @@ using Dada.Models.Profile;
 using Microsoft.AspNetCore.Mvc;
 using Repository.Models;
 using Repository.Repositories.PostRepositories;
+using Repository.Repositories.ProfileRepositories;
 
 namespace Dada.Controllers
 {
@@ -14,19 +15,24 @@ namespace Dada.Controllers
     {
         private readonly IPostRepository _postRepository;
         private readonly IMapper _mapper;
+        private readonly IProfileRepository _profileRepository;
         public PostController(IPostRepository postRepository,
-                              IMapper mapper)
+                              IMapper mapper,
+                              IProfileRepository profileRepository)
         {
             _postRepository = postRepository;
             _mapper = mapper;
+            _profileRepository = profileRepository;
         }
 
         public IActionResult Index(int id)
         {
             var token = HttpContext.Request.Cookies["user-token"];
 
-            ViewBag.token = token;
+            var myprofile = _profileRepository.GetUserByToken(token);
 
+            ViewBag.token = token;
+            ViewBag.user = myprofile;
 
             var post = _postRepository.GetPostById(id);
 
